c1acb0117bb9fc4a68b0e25f6adf0b589f5b8e74
valeriia <izhboldina.valeriia@gmail.com>
2019-01-11 14:40:00 +0300

    can't read all data

--- ver2/flat_plots.py
+++ ver2/flat_plots.py
@@ -5,37 +5,36 @@ from math import *
 d_time = 0.01
 array = []
 accel = []
-ax = []
-ay = []
-az = []
 gx = []
 gy = []
 gz = []
-vel = []
+ax = []
+ay = []
+az = []
 yaw = []
 pitch = []
 roll = []
-v = 0
 
 with open("dt 0_01.txt", 'r') as file:
     data = file.readlines()
     for line in data:
         array.append([float(x) for x in line.split()])
-    num = sum(1 for line in data)
+    # num = sum(1 for line in data)
+    num = 352
     for i in range(0, num):
-        gx.append(array[i][1])
-        gy.append(array[i][2])
-        gz.append(array[i][3])
+        # gx.append(array[i][1])
+        # gy.append(array[i][2])
+        # gz.append(array[i][3])
         ax.append(array[i][4]*(-1))
         ay.append(array[i][5]*(-1))
         az.append(array[i][6])
         # yaw.append(atan(gz[i]/sqrt(gx[i]*gx[i] + gz[i]*gz[i])))
-        yaw.append(array[i][8] * 3.14159  / 180)
+        yaw.append(array[i][8] * 3.14159 * 0.4/ 180)
         pitch.append(array[i][9] * 3.14159 / 180)
         roll.append(array[i][10] * 3.14159 / 180)
         accel.append(np.sqrt(array[i][4] ** 2 + array[i][5] ** 2 + array[i][6] ** 2))
 
-'''plt.plot(ax)
+plt.plot(ax)
 plt.title("Accelaration in X-direction(m/s2)")
 plt.xlabel("time step")
 plt.ylabel("Accelaration")
@@ -52,13 +51,15 @@ plt.title("Accelaration in Z-direction(m/s2)")
 plt.xlabel("time step")
 plt.ylabel("Accelaration")
 plt.show()
-'''
+
+vel = []
+v = 0
 for i in range(0, num):
     v = v + ax[i]*d_time
     vel.append(v)
 vel = np.array(vel)
 
-'''
+
 plt.plot(vel)
 plt.title("Velocity in X-direction(m/s)")
 plt.xlabel("time step")
@@ -70,35 +71,35 @@ plt.title("Length of accel vector")
 plt.xlabel("time step")
 plt.ylabel("Length")
 plt.show()
-'''
+
 dy = vel * d_time
-'''
+
 plt.plot(dy)
 plt.title("Displacement")
 plt.xlabel("time step")
 plt.ylabel("Displacement")
 plt.show()
-'''
+
 dist = []
 d = 0
 for i in range(0, num):
     d = d + dy[i]
     dist.append(d)
-'''
+
 plt.plot(dist)
 plt.title("Total Distance in meter")
 plt.xlabel("time step")
 plt.ylabel("Distance")
 plt.show()
-'''
+
 rot_angle = 2 * np.arcsin(yaw)
-'''
+
 plt.plot(rot_angle)
 plt.title("Rotational Vector in radian")
 plt.xlabel("time step")
 plt.ylabel("angle")
 plt.show()
-'''
+
 x = 0
 y = 0
 x_arr = []
@@ -109,9 +110,9 @@ for i in range(0, num-1):
     x_arr.append(x)
     y_arr.append(y)
 
-plt.scatter(x_arr, y_arr)
+plt.scatter(y_arr, x_arr)
 plt.title("Trajectory ")
-plt.xlabel("y-axis")
-plt.ylabel("x-axis")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
 plt.show()
 
