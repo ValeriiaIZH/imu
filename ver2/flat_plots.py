7e2fb87a873fb3e4f64adc114707349efaf22cc6
valeriia <izhboldina.valeriia@gmail.com>
2019-01-14 16:43:18 +0300

    add new data

--- ver2/flat_plots.py
+++ ver2/flat_plots.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from math import *
 
-d_time = 0.01
+d_time = 0.1
 array = []
 accel = []
 gx = []
@@ -15,24 +15,57 @@ yaw = []
 pitch = []
 roll = []
 
-with open("dt 0_01.txt", 'r') as file:
+with open("sportzal4.txt", 'r') as file:
     data = file.readlines()
     for line in data:
         array.append([float(x) for x in line.split()])
-    # num = sum(1 for line in data)
-    num = 352
+    num = sum(1 for line in data)
     for i in range(0, num):
-        # gx.append(array[i][1])
-        # gy.append(array[i][2])
-        # gz.append(array[i][3])
-        ax.append(array[i][4]*(-1))
-        ay.append(array[i][5]*(-1))
-        az.append(array[i][6])
-        # yaw.append(atan(gz[i]/sqrt(gx[i]*gx[i] + gz[i]*gz[i])))
-        yaw.append(array[i][8] * 3.14159 * 0.4/ 180)
-        pitch.append(array[i][9] * 3.14159 / 180)
-        roll.append(array[i][10] * 3.14159 / 180)
+
+        ax.append(array[i][0])
+        ay.append(array[i][1])
+        az.append(array[i][2])
+
+        gx.append(array[i][3])
+        gy.append(array[i][4])
+        gz.append(array[i][5])
+        yaw.append(atan(gz[i]/sqrt(gx[i]*gx[i] + gz[i]*gz[i])))
+        pitch.append( atan(ax[i] / sqrt(ay[i] * ay[i] + az[i]* az[i])))
+        roll.append( atan(ay[i]/ sqrt(ax[i] * ax[i]+ az[i] * az[i])))
+        #yaw.append(array[i][9] * pi / 180)
+        #pitch.append(array[i][10] * pi / 180)
+        #roll.append(array[i][11] * pi / 180)
         accel.append(np.sqrt(array[i][4] ** 2 + array[i][5] ** 2 + array[i][6] ** 2))
+
+vel = []
+v = 0
+for i in range(0, num):
+    v = v + ay[i]*d_time
+    vel.append(v)
+vel = np.array(vel)
+
+dy = vel * d_time
+
+dist = []
+d = 0
+for i in range(0, num):
+    d = d + dy[i]
+    dist.append(d)
+
+rot_angle = 2 * np.arcsin(yaw)
+#rot_angle = 2 * np.arcsin(pitch)
+#rot_angle = 2 * np.arcsin(roll)
+
+x = 0
+y = 0
+x_arr = []
+y_arr = []
+for i in range(0, num-1):
+    x = x + dy[i+1] * np.sin(rot_angle[i])
+    y = y + dy[i+1] * np.cos(rot_angle[i])
+    x_arr.append(x)
+    y_arr.append(y)
+
 
 plt.plot(ax)
 plt.title("Accelaration x (m/s2)")
@@ -55,14 +88,6 @@ plt.xlabel("time step")
 plt.ylabel("Accelaration")
 plt.show()
 
-vel = []
-v = 0
-for i in range(0, num):
-    v = v + ax[i]*d_time
-    vel.append(v)
-vel = np.array(vel)
-
-
 plt.plot(vel)
 plt.title("Velocity in x (m/s)")
 plt.grid()
@@ -77,20 +102,12 @@ plt.xlabel("time step")
 plt.ylabel("Length")
 plt.show()
 
-dy = vel * d_time
-
 plt.plot(dy)
 plt.title("Displacement")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("Displacement")
 plt.show()
-
-dist = []
-d = 0
-for i in range(0, num):
-    d = d + dy[i]
-    dist.append(d)
 
 plt.plot(dist)
 plt.title("Total distance in meter")
@@ -99,8 +116,6 @@ plt.xlabel("time step")
 plt.ylabel("distance")
 plt.show()
 
-rot_angle = 2 * np.arcsin(yaw)
-
 plt.plot(rot_angle)
 plt.title("Rotational vector in radian")
 plt.grid()
@@ -108,19 +123,8 @@ plt.xlabel("time step")
 plt.ylabel("angle")
 plt.show()
 
-x = 0
-y = 0
-x_arr = []
-y_arr = []
-for i in range(0, num-1):
-    x = x + dy[i+1] * np.sin(rot_angle[i])
-    y = y + dy[i+1] * np.cos(rot_angle[i])
-    x_arr.append(x)
-    y_arr.append(y)
-
-plt.scatter(y_arr, x_arr)
+plt.scatter(x_arr, y_arr )
 plt.title("Trajectory ")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.show()
-
