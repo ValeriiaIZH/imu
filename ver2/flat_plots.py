1867a79e9c2af2686666a2552cdeab7375b75961
valeriia <izhboldina.valeriia@gmail.com>
2019-01-17 14:25:45 +0300

    add real trajectory

--- ver2/flat_plots.py
+++ ver2/flat_plots.py
@@ -20,7 +20,7 @@ yaw = []
 pitch = []
 roll = []
 
-with open("sportzal4.txt", 'r') as file:
+with open("values_from_gym_2.txt", 'r') as file:
     data = file.readlines()
     for line in data:
         array.append([float(x) for x in line.split()])
@@ -44,11 +44,11 @@ with open("sportzal4.txt", 'r') as file:
         #mag_x.append(mx[i] * cos(pitch[i]) + my[i] * sin(roll[i]) * sin(pitch[i]) + mz[i] * cos(roll[i]) * sin(pitch[i]))
         #mag_y.append(my[i] * cos(roll[i]) - mz[i] * sin(roll[i]))
         #yaw.append(atan2(mag_x[i],mag_y[i]))
-        #yaw.append(array[i][9] * pi / 180)
+        yaw.append(array[i][9] * pi / 180)
         if yaw[i] > 1 : yaw[i] = 1
         if yaw[i] < -1 : yaw[i] = -1
         accel.append(np.sqrt(array[i][4] ** 2 + array[i][5] ** 2 + array[i][6] ** 2))
-    print(yaw)
+    # print(yaw)
 
 vel = []
 v = 0
@@ -77,9 +77,26 @@ for i in range(0, num-1):
     x_arr.append(x)
     y_arr.append(y)
 
+# real trajectory
+real_coordinate_x = [0, 0, -6, -6]
+real_coordinate_y = [0, 9.5, 9.5, 0]
 
+'''
+fig, plotting = plt.subplots()
+plotting.set_title('Accelerations (m/s^2)')
+line1, = plotting.plot(ax, color='red', label='x-axis')
+line2, = plotting.plot(ay, color='blue', label='y-axis')
+line3, = plotting.plot(az, color='green', label='z-axis')
+plt.xlabel("time step")
+plt.ylabel("Accelaration")
+leg = plotting.legend(loc='upper left', fancybox=True, shadow=True)
+plt.grid()
+leg.get_frame().set_alpha(0.4)
+plt.show()
+'''
+'''
 plt.plot(ax)
-plt.title("Accelaration x (m/s2)")
+plt.title("Accelaration  (m/s2)")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("Accelaration")
@@ -99,44 +116,58 @@ plt.xlabel("time step")
 plt.ylabel("Accelaration")
 plt.show()
 
-plt.plot(vel)
-plt.title("Velocity in x (m/s)")
-plt.grid()
-plt.xlabel("time step")
-plt.ylabel("Velocity")
-plt.show()
-
 plt.plot(ax)
 plt.title("Length of accel vector")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("Length")
 plt.show()
+'''
 
-plt.plot(dy)
+'''
+plt.plot(vel, color='black')
+plt.title("Velocity in x-axis (m/s)")
+plt.grid()
+plt.xlabel("time step")
+plt.ylabel("Velocity")
+plt.show()
+
+plt.plot(dy, color='black')
 plt.title("Displacement")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("Displacement")
 plt.show()
 
-plt.plot(dist)
+plt.plot(dist, color='black')
 plt.title("Total distance in meter")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("distance")
 plt.show()
 
-plt.plot(rot_angle)
+plt.plot(rot_angle, color='black')
 plt.title("Rotational vector in radian")
 plt.grid()
 plt.xlabel("time step")
 plt.ylabel("angle")
 plt.show()
-
-plt.scatter(x_arr, y_arr )
-plt.title("Trajectory ")
+'''
+'''
+plt.scatter(x_arr, y_arr, color='black')
+plt.title("Trajectory")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
+plt.show()
+'''
+
+fig1, plotting = plt.subplots()
+plotting.set_title('Trajectory')
+plt.scatter(x_arr, y_arr, color='black', label='values from mpu-9265')
+plt.plot(real_coordinate_x, real_coordinate_y, color='blue', label='real trajectory', linewidth=5)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+leg = plotting.legend(loc='lower right', fancybox=True, shadow=True)
+leg.get_frame().set_alpha(0.4)
 plt.show()
 
